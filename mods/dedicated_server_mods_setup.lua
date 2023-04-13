2a31986702a98e00244215c32bbe8f500c29c3fc
root <root@localhost.localdomain>
2023-04-14 01:26:58 +0800

    1

---
+++ mods/dedicated_server_mods_setup.lua
@@ -0,0 +1,69 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+ServerModSetup("375850593")
+ServerModSetup("350811795")
+
+ServerModSetup("661253977")
+
+ServerModSetup("569043634")
+
+ServerModSetup("541537428")
+
+ServerModSetup("447092740")
+
+ServerModSetup("378160973")
+
+ServerModSetup("347079953")
+
+ServerModSetup("2890376601")
+
+ServerModSetup("2807543682")
+
+ServerModSetup("2798599672")
+
+ServerModSetup("2610766467")
+
+ServerModSetup("2577742416")
+
+ServerModSetup("2505341606")
+
+ServerModSetup("2328604820")
+
+ServerModSetup("2208128427")
+
+ServerModSetup("2189004162")
+
+ServerModSetup("2174620235")
+
+ServerModSetup("2119742489")
+
+ServerModSetup("2021943702")
+
+ServerModSetup("1991746508")
+
+ServerModSetup("1951468597")
+
+ServerModSetup("1868741519")
+
+ServerModSetup("1699194522")
+
+ServerModSetup("1595631294")
+
+ServerModSetup("1392778117")
+
+ServerModSetup("1221281706")
+
+ServerModSetup("1207269058")
+
+ServerModSetup("1185229307")
+
+ServerModSetup("2784048339")
