ad0affff8e75b9fafba6cab39487304b6086a0ee
floveram_root <1846666438@qq.com>
2023-04-14 22:40:30 +0800

    2023/4/14 冬天大眼over 春天

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,69 @@
+return {
+  ["workshop-1185229307"]={
+    configuration_options={
+      [""]="",
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      HORIZONTAL_OFFSET=0,
+      NOEPIC=false,
+      NONOEPIC=false,
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-2287303119"]={
+    configuration_options={
+      [""]=0,
+      T_crop=true,
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      item_info_mod=0,
+      lang="Auto",
+      show_buddle_item=1,
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=1, kong=0, nillots=0, rendiao=2, zbdiao=true },
+    enabled=true 
+  } 
+}
--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -7,63 +7,15 @@
 
 --ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
-	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
-	--ServerModCollectionSetup("379114180")
+	--
+ServerModSetup("596633787")
 ServerModSetup("375850593")
-ServerModSetup("350811795")
-
-ServerModSetup("661253977")
-
-ServerModSetup("569043634")
-
-ServerModSetup("541537428")
-
-ServerModSetup("447092740")
-
 ServerModSetup("378160973")
-
-ServerModSetup("347079953")
-
-ServerModSetup("2890376601")
-
-ServerModSetup("2807543682")
-
-ServerModSetup("2798599672")
-
-ServerModSetup("2610766467")
-
-ServerModSetup("2577742416")
-
-ServerModSetup("2505341606")
-
-ServerModSetup("2328604820")
-
-ServerModSetup("2208128427")
-
-ServerModSetup("2189004162")
-
-ServerModSetup("2174620235")
-
-ServerModSetup("2119742489")
-
-ServerModSetup("2021943702")
-
-ServerModSetup("1991746508")
-
-ServerModSetup("1951468597")
-
-ServerModSetup("1868741519")
-
-ServerModSetup("1699194522")
-
-ServerModSetup("1595631294")
-
-ServerModSetup("1392778117")
-
-ServerModSetup("1221281706")
-
+ServerModSetup("375859599")
+ServerModSetup("374550642")
+ServerModSetup("2287303119")
+ServerModSetup("666155465")
 ServerModSetup("1207269058")
-
+ServerModSetup("362175979")
 ServerModSetup("1185229307")
-
-ServerModSetup("2784048339")
+ServerModSetup("661253977")
